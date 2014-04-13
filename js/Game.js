a59d19ace6de54d10cefa70eab7e43478a875cfa
John McDonald <jcpmcdonald@gmail.com>
2014-04-13 14:52:31 -0600

    Phone test 5

--- js/Game.js
+++ js/Game.js
@@ -555,33 +555,32 @@ var Game = {
 }
 
 
-document.addEventListener("deviceready", onDeviceReady, false);
-
-// $(document).ready(function() {
-	// // are we running in native app or in a browser?
-	// window.isphone = false;
-	// if(document.URL.indexOf("http://") === -1 
-		// && document.URL.indexOf("https://") === -1) {
-		// window.isphone = true;
-	// }
-	// 
-	// if( window.isphone ) {
-		// 
-	// } else {
-		// onDeviceReady();
-	// }
-// });
+$(document).ready(function() {
+	// are we running in native app or in a browser?
+	window.isphone = false;
+	if(document.URL.indexOf("http://") === -1 
+		&& document.URL.indexOf("https://") === -1
+		&& document.URL.indexOf("file://") === -1) {
+		window.isphone = true;
+	}
+	
+	if( window.isphone ) {
+		document.addEventListener("deviceready", onDeviceReady, false);
+	} else {
+		onDeviceReady();
+	}
+});
 
 function onDeviceReady() {
-	Game.start();
-	
 	log("System", "onDeviceReady");
 	
-	// if(window.isphone)
-	// {
-		// gaPlugin = window.plugins.gaPlugin;
-		// gaPlugin.init(successHandler, errorHandler, "UA-49961926-1", 10);
-	// }
+	Game.start();
+	
+	if(window.isphone)
+	{
+		gaPlugin = window.plugins.gaPlugin;
+		gaPlugin.init(successHandler, errorHandler, "UA-49961926-1", 10);
+	}
 }
 
 function successHandler()
