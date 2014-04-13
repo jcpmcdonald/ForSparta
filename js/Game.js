47297dc151af286934a0ef47a4a28573304da6db
John McDonald <jcpmcdonald@gmail.com>
2014-04-13 15:05:03 -0600

    Phone test 7, phonegap uses "file://". Removing jQueryUI

--- js/Game.js
+++ js/Game.js
@@ -559,15 +559,14 @@ $(document).ready(function() {
 	// are we running in native app or in a browser?
 	window.isphone = false;
 	if(document.URL.indexOf("http://") === -1 
-		&& document.URL.indexOf("https://") === -1
-		&& document.URL.indexOf("file://") === -1) {
+		&& document.URL.indexOf("https://") === -1) {
 		window.isphone = true;
 	}
 	
 	if( window.isphone ) {
 		document.addEventListener("deviceready", onDeviceReady, false);
 	} else {
-		//onDeviceReady();
+		onDeviceReady();
 	}
 });
 
